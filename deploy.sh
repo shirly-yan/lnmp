6a9d5377fbe8b5e52b48c24f267037d75c5a6300
shirly <yqzh186@163.com>
2019-04-28 16:53:41 +0800

    only mysql

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+chown -R mysql:mysql /var/lib/mysql
 service mysql start
 
-redis-server ./redis.conf
+# redis-server ./redis.conf
 
-service php7.2-fpm start
+# service php7.2-fpm start
 
-echo 'daemon off;' >> '/etc/nginx/nginx.conf'
-rm '/etc/nginx/sites-enabled/default'
-cp './default.conf' '/etc/nginx/conf.d/default.conf'
-service nginx start
+# echo 'daemon off;' >> '/etc/nginx/nginx.conf'
+# rm '/etc/nginx/sites-enabled/default'
+# cp './default.conf' '/etc/nginx/conf.d/default.conf'
+# service nginx start
